cf0e7825031bc85dc0f85149898e837012515f68
zhangweixsw <37978839+zhangweixsw@users.noreply.github.com>
2022-06-20 15:23:55 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,16 +2,16 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: 'd5532b3c2ab34097ae878910ba8acea5',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'My Portfolio!',
+  domain: 'https://fog-gateway-cb8.notion.site/My-Portfolio-d5532b3c2ab34097ae878910ba8acea5',
+  author: '猫头羊',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
